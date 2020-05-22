2a0650020b74d2b4afce0433bb78a428c46fc46a
Gunju-Ko <gunjuko92@gmail.com>
2020-05-22 22:00:12 +0900

    Add Calculator and test

---
+++ src/main/java/hyunyong/learning/Calculator.java
@@ -0,0 +1,22 @@
+package hyunyong.learning;
+
+public class Calculator {
+
+    private final int operand1;
+    private final int operand2;
+
+    public Calculator(int operand1, int operand2) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+    }
+
+    public int add() {
+        // TODO: 두 수의 합을 리턴
+        return 0;
+    }
+
+    public int minus() {
+        // TODO: 두 수의 차를 리턴
+        return 0;
+    }
+}
---
+++ src/test/java/hyunyong/learning/CalculatorTest.java
@@ -0,0 +1,25 @@
+package hyunyong.learning;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        Calculator calculator = new Calculator(3, 5);
+
+        int result = calculator.add();
+
+        assertEquals(result, 8);
+    }
+
+    @Test
+    void minus() {
+        Calculator calculator = new Calculator(3, 5);
+
+        int result = calculator.minus();
+
+        assertEquals(result, -2);
+    }
+}
