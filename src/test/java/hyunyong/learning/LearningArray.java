3c6b9aac94679bb9d727c37706bea90486d97118
hyunyong92 <gusdyddlwmf@gmail.com>
2020-05-29 16:50:27 +0900

    Array 조건문 문제풀이

--- src/test/java/hyunyong/learning/LearningArray.java
+++ src/test/java/hyunyong/learning/LearningArray.java
@@ -7,18 +7,23 @@ class LearningArray {
 
     @Test
     void testSumOfOdd() {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] nums = {1, 2, 3, 4, 7, 6, 7, 8, 9, 10};
 
         int result = sumOfAdd(nums);
 
-        Assertions.assertEquals(result, 1 + 3 + 5 + 7 + 9);
+        Assertions.assertEquals(result, 1 + 3 + 7 + 7 + 9);
     }
 
     private int sumOfAdd(int[] nums) {
         // 홀수의 합을 리턴한다.
-        return 0;
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                result = result + nums[i];
+            }
+        }
+        return result;
     }
-
     @Test
     void testDivisor() {
         int[] nums = {5, 9, 7, 10};
@@ -32,6 +37,12 @@ class LearningArray {
     private int getCountOfDividingNumber(int[] nums, int divisor) {
         // divisor로 나누어 떨어지는 수의 개수를 리턴하세요.
         // 예를 들어 5, 9, 7, 10에서 5로 나누어 떨어지는 수는 5와 10 이므로 2를 리턴
-        return 0;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] %  5 == 0)
+                count++;
+        }
+
+        return count;
     }
 }
