98a4997c87475b0d86f83e03fa8ce81ea34c9d69
Gunju-Ko <gunjuko92@gmail.com>
2020-06-04 20:38:39 +0900

    formatting code

--- src/main/java/hyunyong/learning/Dictionary.java
+++ src/main/java/hyunyong/learning/Dictionary.java
@@ -1,6 +1,7 @@
 package hyunyong.learning;
 
 public class Dictionary {
+
     private final String[] words;
 
     public Dictionary(String[] words) {
@@ -8,17 +9,15 @@ public class Dictionary {
     }
 
     public int find(String word) {
-        // TODO: implementation
         for (int i = 0; i < words.length; i++) {
-            if (words[i].equals(word))
+            if (words[i].equals(word)) {
                 return i;
+            }
         }
         return -1;
-
     }
 
     public String get(int index) {
-        // TODO: implementation
-    return words[index];
+        return words[index];
     }
 }
