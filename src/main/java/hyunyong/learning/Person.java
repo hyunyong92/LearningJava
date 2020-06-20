d71420d24bfb2904b4fde8e8c93d662f04ce251b
Gunju-Ko <gunjuko92@gmail.com>
2020-06-20 14:19:58 +0900

    Person 클래스 생성 및 테스트 추가

--- src/main/java/hyunyong/learning/Person.java
+++ src/main/java/hyunyong/learning/Person.java
@@ -2,8 +2,14 @@ package hyunyong.learning;
 
 public class Person {
     private String name;
+    // TODO : Person에다가 age을 추가한다. (필드) age는 int. 값을 setter, getter
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        // TODO : implement
+        return null;
     }
 }
--- src/test/java/hyunyong/learning/PersonTest.java
+++ src/test/java/hyunyong/learning/PersonTest.java
@@ -1,15 +1,23 @@
 package hyunyong.learning;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class PersonTest {
 
     @Test
     void testClass() {
+        // hyunyong age 29
         Person hyunyong = new Person();
         hyunyong.setName("hyunyong");
 
+        // gunju age 28
         Person gunju = new Person();
         gunju.setName("gunju");
+
+        Assertions.assertEquals("hyunyong", hyunyong.getName());
+        Assertions.assertEquals("gunju", gunju.getName());
+        Assertions.assertEquals(29, hyunyong.getAge());
+        Assertions.assertEquals(28, gunju.getAge());
     }
 }
